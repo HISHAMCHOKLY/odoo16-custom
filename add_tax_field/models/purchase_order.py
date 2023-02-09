6d5ed1d6f00e207e0bd08e1c55bb1035e7e122ff
HISHAMCHOKLY <hisham.c95@gmail.com>
2023-02-09 11:59:47 +0530

    custom untaxed amount

--- add_tax_field/models/purchase_order.py
+++ add_tax_field/models/purchase_order.py
@@ -1,42 +1,35 @@
 from odoo import api, fields, models, exceptions
+from odoo.tools.misc import formatLang
 
 
 class PurchaseOrder(models.Model):
     _inherit = 'purchase.order'
 
+    excise_tax_tot = fields.Float(string="Excise Tax")
+
+
     @api.depends('order_line.price_total')
     def _compute_grand_total(self):
-        # print(self.amount_untaxed)
-        # print(self.total_tax)
-
-        record = self.env['purchase.order'].search([],limit=1, order='id desc')
-        print(record)
-        tax_totals = {'amount_untaxed': 999.0, 'amount_total': 999.0, 'formatted_amount_total': '₹\xa0999.00','formatted_amount_untaxed': '₹\xa0999.00', 'subtotals': [], 'subtotals_order': [], 'display_tax_base': False}
-
-        # self.tax_totals.update({'tax_totals': tax_totals})
-        print(self.tax_totals['amount_total'])
-
-        # print(self.tax_totals['groups_by_subtotal'])
-        # self.tax_totals = {'amount_total': new_value}
-        # self.write({'tax_totals': self.tax_totals})
-        # self.total_tax=self.amount_total-self.amount_untaxed
-
         for order in self:
-            order.grand_total = sum(line.price_total for line in order.order_line)
-            # order.excise_tax = sum(line.excise_tax for line in order.order_line)
-            order.total_tax = sum(line.price_tax for line in order.order_line)
-            order.untaxed_amount = sum(line.price_subtotal for line in order.order_line)
-        self.tax_totals['amount_total']=1222222222222222
-        self.tax_totals['price_tax']=1222222222222222
-        print(self.tax_totals['amount_total'])
-        print(self.tax_totals['price_tax'])
+            order_lines = order.order_line.filtered(lambda x: not x.display_type)
+            order.excise_tax = sum(order_lines.mapped('excise_tax'))
+            order.total_tax = sum(order_lines.mapped('price_tax'))
+            order.untaxed_amount = sum(order_lines.mapped('price_subtotal')) - sum(order_lines.mapped('excise_tax'))
 
-        # self.tax_totals['amount_total']=self.grand_total
+    excise_tax = fields.Float(compute='_compute_grand_total', store=True)
+    untaxed_amount = fields.Float(compute='_compute_grand_total', store=True)
+    total_tax = fields.Float(string="Taxes", compute='_compute_grand_total', store=True)
 
-    grand_total = fields.Float(compute='_compute_grand_total', store=True)
-    # excise_tax = fields.Float(compute='_compute_grand_total', store=True)
-    untaxed_amount=fields.Float(compute='_compute_grand_total', store=True)
-    total_tax = fields.Float(string="Total Tax", compute='_compute_grand_total', store=True)
+    def _prepare_invoice(self):
+        invoice_vals = super()._prepare_invoice()
+        invoice_vals['excise_tax'] = self.excise_tax
+        invoice_vals['untaxed_amount'] = self.untaxed_amount
+        invoice_vals['total_tax'] = self.total_tax
+        invoice_vals['amount_total'] = self.amount_total+self.excise_tax
+        return invoice_vals
+
+
+
 
 
 class PurchaseOrderLine(models.Model):
@@ -49,8 +42,8 @@ class PurchaseOrderLine(models.Model):
         for line in self:
             tax_results = self.env['account.tax']._compute_taxes([line._convert_to_tax_base_line_dict()])
             totals = list(tax_results['totals'].values())[0]
-            amount_untaxed = totals['amount_untaxed']
-            amount_tax = totals['amount_tax'] + self.excise_tax
+            amount_untaxed = totals['amount_untaxed'] + self.excise_tax
+            amount_tax = totals['amount_tax']
 
             line.update({
                 'price_subtotal': amount_untaxed,
@@ -58,8 +51,16 @@ class PurchaseOrderLine(models.Model):
                 'price_total': amount_untaxed + amount_tax,
             })
 
-
     def _prepare_account_move_line(self, move=False):
         res = super()._prepare_account_move_line()
         res.update({'excise_tax': self.excise_tax})
+
         return res
+
+
+
+
+
+
+
+
